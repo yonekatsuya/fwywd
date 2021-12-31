9234910fbf86d1586203cc74642e5a4d47c069b3
yonekatsuya <seiryo.soccer.ky@i.softbank.jp>
2021-12-31 17:43:12 +0900

    #3 フッターコンポーネントの作成

--- self-introduce-site/layouts/Footer.js
+++ self-introduce-site/layouts/Footer.js
@@ -1,9 +1,27 @@
 import React, {Component} from 'react';
 
 class Footer extends Component {
+
     render () {
         return (<>
-            temp
+            <div className="h-80 text-white" style={{'backgroundColor': '#262c3a'}}>
+                <div className="pt-5 text-center">
+                    <div className='inline-block'>
+                        <span className="text-xs table-cell align-middle pr-3">CREATED BY</span>
+                        <span className="text-lg table-cell align-middle">米 勝矢</span>
+                    </div>
+                </div>
+                <div className="h-32 mt-8 text-center">
+                    <img src='logo_white.png' className="w-24 inline-block" />
+                </div>
+                <div className="mt-7 text-center">
+                    <img src="twitter.png" className="w-5 inline-block mr-4" />
+                    <img src="facebook.png" className="w-5 inline-block" />
+                </div>
+                <div className="mt-5 text-center text-xs">
+                    © 2021 KIKAGAKU
+                </div>
+            </div>
         </>);
     }
 }
