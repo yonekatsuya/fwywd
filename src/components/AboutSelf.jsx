ae3e91869f8d7a8670940c7c9b1e175eea8a0889
yonekatsuya <seiryo.soccer.ky@i.softbank.jp>
2022-01-08 19:38:47 +0900

    クラスコンポーネントから関数コンポーネントに変更する

--- src/components/AboutSelf.jsx
+++ src/components/AboutSelf.jsx
@@ -1,38 +1,23 @@
 import Image from 'next/image';
-import React, { Component } from 'react';
 
-class AboutSelf extends Component {
-  constructor(props) {
-    super(props);
-    this.img = props.img;
-    this.title = props.title;
-    this.content = props.content;
-    this.width = props.width;
-  }
-
-  render() {
-    return (
-      <>
-        <div className='w-2/6'>
-          <div className='text-center'>
-            <Image
-              src={this.img}
-              width={this.width}
-              height={140}
-              objectFit='contain'
-              alt='私を表す画像'
-            />
-          </div>
-          <div className='pt-5 text-xl font-bold tracking-widest text-center text-text-green'>
-            {this.title}
-          </div>
-          <div className='flex justify-center'>
-            <div className='pt-4 pl-2 w-11/12'>{this.content}</div>
-          </div>
-        </div>
-      </>
-    );
-  }
+export default function AboutSelf(props) {
+  return (
+    <div className='w-2/6'>
+      <div className='text-center'>
+        <Image
+          src={props.img}
+          width={props.width}
+          height={140}
+          objectFit='contain'
+          alt='私を表す画像'
+        />
+      </div>
+      <div className='pt-5 text-xl font-bold tracking-widest text-center text-text-green'>
+        {props.title}
+      </div>
+      <div className='flex justify-center'>
+        <div className='pt-4 pl-2 w-11/12'>{props.content}</div>
+      </div>
+    </div>
+  );
 }
-
-export default AboutSelf;
